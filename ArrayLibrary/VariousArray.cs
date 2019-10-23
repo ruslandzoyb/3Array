f79e9e220e2ededf526646378bed467eeac85445
rusya <dnepr.ukrainec@gmail.com>
2019-10-23 20:59:39 +0300

    Добавьте файлы проекта.

---
+++ ArrayLibrary/VariousArray.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayLibrary
+{
+    public class VariousArray<T> :IEnumerable<T>,IEnumerator<T>
+    {
+        private List<T> list = new List<T>();
+        int position = -1;
+
+        public T Current { get { return list[position]; } }
+
+        object IEnumerator.Current { get { return list; } }
+
+       public void Add(object obj)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+           
+            list.Add((T)obj);
+          
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return list.GetEnumerator();
+            throw new NotImplementedException();
+        }
+
+        public bool MoveNext()
+        {
+            if (position<list.Capacity-1)
+            {
+                position++;
+                return true;
+            }
+            return false;
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+       
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArrayLibrary;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VariousArray<object> array = new VariousArray<object>();
+
+            array.Add("str");
+            array.Add(3);
+
+          
+           
+            foreach (var item in array)
+            {
+                Console.WriteLine($"{item} ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
